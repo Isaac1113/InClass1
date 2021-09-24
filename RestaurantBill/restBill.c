2131c649a2616b14953744bbd311e867e642a405
Isaac Diaz <itdiaz@cpp.edu>
2021-09-23 20:16:07 -0700

    Add include files

--- RestaurantBill/restBill.c
+++ RestaurantBill/restBill.c
@@ -1,6 +1,6 @@
 /*
     First, need to include stdio.h, sidlib.h, and time.h to use input/output, and generate random numbers.
-    Create main with int argc and chr *argv[].
+    Create main with int argc and char *argv[].
     Create variable ot time_t element.
     Create an array of type double with meal cost
     Initiate the random number generator using srand and time_t variable
@@ -11,3 +11,7 @@
     Display the total bill by adding the meal cost, tax amount, and tip amount.
     End Program
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
