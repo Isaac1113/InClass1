0d4e63165a0faa886f04e376a986c5fa75ff6b06
Isaac Diaz <itdiaz@cpp.edu>
2021-09-23 19:17:36 -0700

    Add outline to restBill.c

---
+++ RestaurantBill/restBill.c
@@ -0,0 +1,13 @@
+/*
+    First, need to include stdio.h, sidlib.h, and time.h to use input/output, and generate random numbers.
+    Create main with int argc and chr *argv[].
+    Create variable ot time_t element.
+    Create an array of type double with meal cost
+    Initiate the random number generator using srand and time_t variable
+    Create a double variable and assign it a random value from the array
+    Display the meal cost.
+    Display the tax amount by multiplying meal cost by the tax percent from the command line.
+    Display the tip amount by multiplying meal cost by the tip percent from the command line.
+    Display the total bill by adding the meal cost, tax amount, and tip amount.
+    End Program
+*/
